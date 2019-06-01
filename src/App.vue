1b16dbe3963f0c452e77065850f830bad6400d0c
625dennis <denniskuduzovic@gmail.com>
2019-06-01 12:51:45 -0600

    Start family tree implementation with sigma.js

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,31 @@
-<template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+<template lang="pug">
+  #app
+    FamilyTree
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import FamilyTree from './components/FamilyTree.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    FamilyTree
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+	body {
+		margin: 0;
+	}
+	
+	/*
+	#app {
+	  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+	  -webkit-font-smoothing: antialiased;
+	  -moz-osx-font-smoothing: grayscale;
+	  text-align: center;
+	  color: #2c3e50;
+	}
+	*/
 </style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
 import Vue from 'vue'
+import VueGoogleCharts from 'vue-google-charts'
+
 import App from './App.vue'
 import store from './store'
+
+Vue.use(VueGoogleCharts)
 
 Vue.config.productionTip = false
 
