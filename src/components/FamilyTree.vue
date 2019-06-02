4a1e738a4f9424f32936044162eb3820f4e57d5a
625dennis <denniskuduzovic@gmail.com>
2019-06-02 14:11:31 -0600

    Fix placement of family members using generation count

---
+++ src/components/FamilyTree.vue
@@ -0,0 +1,86 @@
+<template lang="pug">
+	#family-tree
+</template>
+
+<script>
+	import Sigma from 'sigma'
+	
+	let familyTree
+	
+	let countAncestors 
+	countAncestors = (family, name) => {
+		const [child, parents = []] = family.find(([child]) => child === name)
+		
+		const counts = parents.map(parent => countAncestors(family, parent))
+		const greatest = counts.reduce((greatest, count) => count > greatest ? count : greatest, 0)
+		
+		return 1 + greatest
+	}
+	
+	const countGeneration = (family, generation) =>	family
+		.map(([member]) => countAncestors(family, member))
+		.filter(count => count === generation)
+		.length
+	
+	const createFamilyTree = (tree, familyData) => {
+		const { graph } = tree
+		const addedFamily = []
+	
+		familyData.forEach(([child, parents]) => {
+			const generation = countAncestors(familyData, child)
+		
+			graph.addNode({
+				id: child,
+				label: child,
+				x: countGeneration(addedFamily, generation) / 4,
+				y: generation / 4,
+				size: 1,
+				color: '#f00'
+			})
+			
+			addedFamily.push([child, parents])
+			
+			if(parents) {
+				parents.forEach(parent => {
+					graph.addEdge({
+						id: `${parent} - ${child}`,
+						source: parent,
+						target: child
+					})
+				})
+			}
+		})
+		
+		tree.refresh()
+	}
+
+	export default {
+		mounted() {
+			familyTree = new Sigma('family-tree')
+			
+			createFamilyTree(familyTree, this.family)
+		},
+		data() {
+			return {
+				family: [
+					['Muhamed'],
+					['Redzima'],
+					['Todor'],
+					['Pera'],
+					['Besim', ['Muhamed', 'Redzima']],
+					['Cedna', ['Todor', 'Pera']],
+					['Dennis', ['Besim', 'Cedna']],
+					['Nina', ['Besim', 'Cedna']]
+				]
+			}
+		}
+	}
+</script>
+
+<style scoped>
+	#family-tree {
+		position: absolute;
+		height: 100%;
+		width: 100%;
+	}
+</style>
